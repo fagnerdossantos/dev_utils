4eef04ff7092b8835f3e97c74d978b3d184acc6a
fagnerdossantos <61123877+fagnerdossantos@users.noreply.github.com>
2023-02-20 16:54:24 -0300

    Fixing main
    Remove main.

--- lib/main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dev_utils/dev_utils.dart';
-
-void main() async {
-  /* CPF */
-  final cpfController = CPFController();
-  Map<String, dynamic> cpf = cpfController.generate();
-  print(cpf);
-}
